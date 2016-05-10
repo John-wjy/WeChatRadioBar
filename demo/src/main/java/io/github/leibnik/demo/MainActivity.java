016a6aa3e5b65a956a4c14c82ddf2d5c65e61c29
leibnik <860321158@qq.com>
2016-05-10 15:15:15 +0800

    WechatRadioBar by leibnik

---
+++ demo/src/main/java/io/github/leibnik/demo/MainActivity.java
@@ -0,0 +1,60 @@
+package io.github.leibnik.demo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.leibnik.wechatradiobar.WechatRadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private WechatRadioGroup gradualRadioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        gradualRadioGroup = (WechatRadioGroup) findViewById(R.id.radiogroup);
+        List<DemoFragment> list = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            DemoFragment fragment = new DemoFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("type",i);
+            fragment.setArguments(bundle);
+            list.add(fragment);
+        }
+        viewPager.setAdapter(new DemoPagerAdapter(getSupportFragmentManager(), list));
+        gradualRadioGroup.setViewPager(viewPager);
+    }
+
+    class DemoPagerAdapter extends FragmentPagerAdapter {
+        List<DemoFragment> mData;
+
+        public DemoPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public DemoPagerAdapter(FragmentManager fm, List<DemoFragment> data) {
+            super(fm);
+            mData = data;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mData.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mData.size();
+        }
+    }
+}
